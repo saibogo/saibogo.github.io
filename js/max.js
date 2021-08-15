cc0f7a56d8e86637d46da8afeb86ebdc705e863a
Andrey Gleykh <gleykh@gmail.com>
2021-08-15 09:40:59 +0500

    Add one photo in Max-page

--- js/max.js
+++ js/max.js
@@ -1,14 +1,22 @@
 var slideIndex = 0;
-var numbersPhotos = 2;
+var numbersPhotos = 3;
 var replaceInterval = 8000;
-let captions = ["Жил-был рыжий кот...", "Любил, чтобы ему чесали пузяку"];
+var typingInterval = 200;
+let captions = ["Жил-был рыжий кот", "Любил, чтобы ему чесали пузяку", "Хулиганил по мере возможностей"];
 
 function showSlides() {
 
     var slide = document.getElementById("photo");
     var nameFile = "img/max" + (slideIndex + 1) + ".jpg";
-    document.getElementsByTagName('h2')[0].innerHTML = captions[slideIndex];
+    loop_print(captions[slideIndex]);
     slide.setAttribute("src", nameFile);
     slideIndex = (slideIndex + 1) % numbersPhotos;
     setTimeout(showSlides, replaceInterval);
+}
+
+function loop_print(pattern) {
+    for(let i = 0; i < pattern.length; i++) {
+        document.getElementsByTagName('h2')[0].innerHTML = pattern.slice(0, i + 1);
+        sleep(typingInterval);
+    }
 }
